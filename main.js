3c51cb324bc55ed534994c7e6792a14af75b6a4b
TC McCarthy <tc.mccarthy1@gmail.com>
2022-03-07 19:48:51 -0500

    Everything works!

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// silence
+const select = document.querySelector("select.filter");
+
+select.addEventListener("change", function (e) {
+  const filter = e.target.value;
+
+  const actives = document.querySelectorAll(".item.active");
+
+  actives.forEach(function (item) {
+    item.classList.remove("active");
+  });
+
+  const make_active = document.querySelectorAll(`.item.${filter}`);
+
+  make_active.forEach(function (item) {
+    item.classList.add("active");
+  });
+});
